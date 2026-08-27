6ba027d02a503af5cc5c48a585eeb631b57926c1
princekgupta0000000000-coder <princekgupta0000000000@gmail.com>
2026-08-27 11:05:15 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/jarvis/MainActivity.kt
+++ app/src/main/java/com/example/jarvis/MainActivity.kt
@@ -42,6 +42,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.jarvis.ui.chat.ChatScreen
 import kotlin.math.cos
 import kotlin.math.sin
 
